a113561fc20fc629d582776325c2f36d7b54ab61
cleroux <cleroux84@gmail.com>
2020-07-07 11:10:56 +0200

    debug

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PaymentController extends AbstractController
 {
+
     /**
      * @Route("/payment/{id}", name="payment")
      */
@@ -34,14 +35,12 @@ class PaymentController extends AbstractController
      */
    public function newPayment(Request $request, Campaign $campaign): Response
     {
-        $participant = new Participant();
-        $amount = $request->request->get('amount');
         $entityManager = $this->getDoctrine()->getManager();
-
+        $participant = new Participant();
+        
 
         $name = $request->request->get('name');
         $email = $request->request->get('email');
-        
         $participant->setName($name)
                     ->setEmail($email)
                     ->setCampaign($campaign);
@@ -50,49 +49,22 @@ class PaymentController extends AbstractController
         
         $payments = new Payment();
         $amount = $request->request->get('amount');
+      
+
+        if($amount > 0)
+        {
         $payments->setAmount($amount)
                 ->setParticipant($participant)
                 ->setCreatedAt(new \DateTime());
         $entityManager->persist($payments);
         $entityManager->flush();  
+        }
+        else{
+            dd($amount);
+        }
             
         return $this->redirectToRoute('campaign_index');;
     } 
-/**
-     * @Route("/campaign/{id}", name="campaigneuh")
-     */
 
+   
 }
-
-/**
-     * @Route("/payment", name="payment")
-     */
-/* 
- public function index()
-    {
-        $participants = $this->getDoctrine()
-            ->getRepository(Participant::class)
-            ->findAll();
-        $payments = $this->getDoctrine()
-            ->getRepository(Payment::class)
-            ->findAll();
-        $campaigns = $this->getDoctrine()
-            ->getRepository(Campaign::class)
-            ->findAll();    
-        return $this->render('payment/index.html.twig', [
-            'controller_name' => 'PaymentController',
-            'participants' => $participants,
-            'payments' => $payments,
-        ]); */
-
-/*     public function new(Request $request): Response
-    {
-        $payments = new Payment();
-       if(null!==('SubmitPayment')){
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($payments);
-            $entityManager->flush();
-       }
-
-            return $this->redirectToRoute('payment/index.html.twig');
-        } */
